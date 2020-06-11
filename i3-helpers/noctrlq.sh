f65316b9fec5d66abec8148367a3536ce0059e8c
SabbathHex <sh@93546bd8-f8c4-40e9-a27a-3bd09ba47648>
2020-06-11 12:00:21 -0700

    added nohups for launched programs

--- i3-helpers/noctrlq.sh
+++ i3-helpers/noctrlq.sh
@@ -5,5 +5,7 @@
 W=$(xdotool getactivewindow)
 S1=$(xprop -id "${W}" |awk -F '"' '/WM_CLASS/{print $4}')
 if [ "$S1" != "Firefox" ] && [ "$S1" != "Firefox Developer Edition" ]; then
-        xvkbd -xsendevent -text "\Cq"
+    xvkbd -xsendevent -text "\Cq"
+elif [ "$S1" = "pavucontrol" ]; then
+    xvkbd -xsendevent -text "\C\Aq"
 fi
